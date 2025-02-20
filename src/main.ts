0bf4874d7ffe7e160603a87bf341042ad48c3c00
Andrey Lechev <andrey@lechev.me>
2025-02-20 18:56:20 +0100

    Add Swagger documentation (will update the design later)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Wanna Wanna API')
+    .setDescription('Wanna Wanna API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().catch((err) => console.error('Failed to start application:', err));
